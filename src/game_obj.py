b3aa33e120644a3cb6cb31ad72bfa5e3944a0d79
Kamil Kandefer <kamil.0392@gmail.com>
2017-04-22 13:47:25 +0200

    Add game object base class

---
+++ src/game_obj.py
@@ -0,0 +1,16 @@
+'''
+Created on 22.04.2017
+
+@author: Kamil
+'''
+
+class GameObj(object):
+
+    def __init__(self, params):
+        pass
+        
+    def draw(self):
+        pass
+    
+    def update(self):
+        pass
--- src/main.py
+++ src/main.py
@@ -10,8 +10,13 @@ if __name__ == '__main__':
     size = width, height = 320, 240
     screen = pygame.display.set_mode(size)
     
+    ball_pos = (100,100)
+        
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
+    
+        screen.fill((0,0,0))
+        pygame.draw.circle(screen, (255,0,0), ball_pos, 30, 10)
     
         pygame.display.flip()
